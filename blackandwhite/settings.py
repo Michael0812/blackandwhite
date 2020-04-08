da1cb7423a59f3cdfff83e6e9e6541ce7f7ce092
Michael0812 <michalwesolowski11@gmail.com>
2020-04-08 14:45:06 +0000

    added new allowed host in settings.py

--- blackandwhite/settings.py
+++ blackandwhite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8080-e176f289-4742-4090-baaa-54a60c065751.ws-eu01.gitpod.io']
+ALLOWED_HOSTS = ['8080-e176f289-4742-4090-baaa-54a60c065751.ws-eu01.gitpod.io', 'black-a-white.herokuapp.com']
 
 
 # Application definition

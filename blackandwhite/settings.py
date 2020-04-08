1b72bd126417d3ef6061a1717b2cfcae38bd013e
Michael0812 <michalwesolowski11@gmail.com>
2020-04-08 12:45:32 +0000

    added new allowed host

--- blackandwhite/settings.py
+++ blackandwhite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8080-e176f289-4742-4090-baaa-54a60c065751.ws-eu01.gitpod.io']
 
 
 # Application definition

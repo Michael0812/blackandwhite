536d8b296717c57e3e5947251f2814259d95ef99
Michael0812 <michalwesolowski11@gmail.com>
2020-04-18 12:30:17 +0000

    added pages in urlpatterns

--- blackandwhite/urls.py
+++ blackandwhite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages.views import index
 from django.template import Template
 
@@ -22,4 +22,5 @@ from django.template import Template
 urlpatterns = [
     path('', index, name="home"),
     path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls')),
 ]

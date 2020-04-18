e86cd3e3c745bfcf791e59d275a97c17a6e8b601
Michael0812 <michalwesolowski11@gmail.com>
2020-04-18 12:32:38 +0000

    created views for delivery information page

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,10 @@ def index(request):
     A view that displays the index page
     """
     return render(request, 'index.html')
+
+
+def delivery(request):
+    """
+    a view that displays page with delivery information
+    """
+    return render(request, 'delivery.html')

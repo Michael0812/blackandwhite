95840e4d1820361f4d83d25488a434bbe1af1567
Michael0812 <michalwesolowski11@gmail.com>
2020-04-18 13:32:31 +0000

    created contact page & added urls, views

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import delivery, terms
+from .views import delivery_view, terms_view, contact_view
 
 
 urlpatterns = [
-   path('delivery/', delivery, name="delivery"),
-   path('terms/', terms, name="terms"),
+   path('delivery/', delivery_view, name="delivery"),
+   path('terms/', terms_view, name="terms"),
+   path('contact/', contact_view, name="contact"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,24 +2,30 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def index_view(request):
     """
     A view that displays the index page
     """
     return render(request, 'index.html')
 
 
-def delivery(request):
+def delivery_view(request):
     """
     a view that displays page with delivery information
     """
     return render(request, 'delivery.html')
 
 
-def terms(request):
+def terms_view(request):
     """
     a view that displays page with terms & conditions
     """
     return render(request, 'terms.html')
 
 
+def contact_view(request):
+    """
+    Renders contact form on contact page with any relevant information
+    the user has already provided in the name and email fields.
+    """
+    return render(request, 'contact.html')

d1026c0cd7e9c745463c168b6ed905a8e707e19e
Michael0812 <michalwesolowski11@gmail.com>
2020-05-25 22:03:21 +0000

    changed model for order

--- checkout/models.py
+++ checkout/models.py
@@ -7,13 +7,12 @@ class Order(models.Model):
     Created model for one order
     """
     full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
-    county = models.CharField(max_length=40, blank=False)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=40, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+
     date = models.DateField()
 
     def __str__(self):
